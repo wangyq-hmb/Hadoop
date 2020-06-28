91ee5387ef65f9c61d36e6d8df7dd7ff84495e66
wangyq-hmb <67536571+wangyq-hmb@users.noreply.github.com>
2020-06-28 15:57:47 +0800

    Create Basic.java

---
+++ Basic.java
@@ -0,0 +1,44 @@
+public class Basic {
+    public static void main(String[] args) {
+        System.out.println("I love you");
+    }
+}
+    /* public static void main(String[] args) {
+         System.out.println("宿舍");
+         Scanner scanner = new Scanner(System.in);
+       /*  try {
+             Thread.sleep(1000);
+         } catch (InterruptedException e) {
+             e.printStackTrace();
+         }*/
+    //让程序睡一会
+    //会吵
+     /*   double random = Math.random();//随机数
+        System.out.println(random);*/
+    //   int random = (int) (Math.random() * 20 + 50);
+    //  System.out.println(random);*/
+//    public static void main(String[] args) {
+//        for (int i = 1; i <= 9; i++) {
+//          for (int j = 1; j <= i; j++) {
+//                System.out.print(j + "*" + i + "=" + j * i + "\t");
+//            }
+//            System.out.println();
+//        }
+//    }
+//}
+    //while 和 do while 的区别使用
+//  定义 条件变量  进入的条件 变量的改变
+    // 变量的改变是为了满足退出条件
+    // do while 无论如何都要先执行下，然后才去判断
+    // while 满足条件才能进入
+    // break 和 continue
+    //  for循环可以打标签，使用break + 标签名可以退出被打标签的循环
+   // flag:for(int i = 1;i<2;i++){
+   //     for (int j = 1; j <= 2; j++) {
+//            if (j > 0) {
+//                break flag;
+//            }
+//            System.out.println("=====" + j);
+//        }
+//    }
+//虽然用的少，但是很重要
